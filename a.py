0350a35e08f268be379ff6225564c762535fc082
Yali <guaidoukx@gmail.com>
2021-10-22 00:31:03 +0800

    token

--- a.py
+++ a.py
@@ -1,10 +1,10 @@
 import os
-import json
 
 token = os.getenv("token")
 tenantid = os.getenv("tenantid")
 token1 = os.getenv("token1")
 
-print(json.decoder(token))
+
+print(eval(token))
 print(tenantid)
 print(token1)
